3d6eff6d0fdde841d71f0233e31bebbd1c047db4
ZRoman87 <zrazhevskiy.roman@yandex.ru>
2023-01-11 21:34:36 +0300

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+
+        System.out.println();
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+        System.out.println();
+    }
+
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+        System.out.println();
+    }
+
+
+    public static void task4() {
+        System.out.println("Задача 4");
+
+        System.out.println();
+    }
+
+
+    public static void task5() {
+        System.out.println("Задача 5");
+
+        System.out.println();
+    }
+}
+
